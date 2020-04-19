79feda3bbef94e806d0e0139f6171222835f2853
Alex <aleks.ce267@gmail.com>
2020-04-20 00:16:34 +0300

    Added webserver.

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,21 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <../lib/Time-master/TimeLib.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 
 #define ARRAY_LENGTH 5
-#define STRING_LENGTH 50
-#define WORKING_PERIOD 30000
-#define SLEEPING_PERIOD 30000
-#define DEBUG false
+#define WORKING_PERIOD 30 * 1000
+#define SLEEPING_PERIOD 30 * 1000
+const int SENSOR_RX_PIN = 4;
+const int SENSOR_DX_PIN = 5;
 
-int rxPin = 4;
-int txPin = 5;
-SdsDustSensor sds(rxPin, txPin);
+SdsDustSensor sds(SENSOR_RX_PIN, SENSOR_DX_PIN);
 int currentReadingIndex = -1;
-boolean shiftArray = false;
+boolean firstPass = true;
+
+MDNSResponder mdns;
+ESP8266WebServer server(80);
 
 struct Measure {
     time_t measureTime;
@@ -23,20 +26,26 @@ struct Measure {
 
 struct Measure measures[ARRAY_LENGTH];
 
-void printMeasure(Measure measure) {
-    time_t currTime = measure.measureTime;
-    Serial.printf("%02d", hour(currTime));
-    Serial.print(":");
-    Serial.printf("%02d", minute(currTime));
-    Serial.print(":");
-    Serial.printf("%02d", second(currTime));
-    Serial.print(" - ");
-    Serial.print("PM2.5 = ");
-    Serial.printf("%.1f", measure.pm25);
-    Serial.print(", ");
-    Serial.print("PM10 = ");
-    Serial.printf("%.1f", measure.pm10);
-    Serial.println();
+String measureToString(Measure measure) {
+    char measureString[50];
+    snprintf(measureString, 50, "%02d:%02d:%02d - PM2.5 = %.1f, PM10 = %.1f\n",
+            hour(measure.measureTime),
+            minute(measure.measureTime),
+            second(measure.measureTime),
+            measure.pm25, measure.pm10
+    );
+    return String(measureString);
+}
+
+String measuresToString() {
+    String measuresString = "";
+    for (auto &measure : measures) {
+        time_t currTime = measure.measureTime;
+        if (currTime != -1) {
+            measuresString += measureToString(measure);
+        }
+    }
+    return measuresString;
 }
 
 void setup() {
@@ -46,16 +55,37 @@ void setup() {
     Serial.println(sds.queryFirmwareVersion().toString()); // prints firmware version
     Serial.println(sds.setQueryReportingMode().toString()); // ensures sensor is in 'query' reporting mode
 
-    WiFi.begin("ALEKSNET", "ekvatorthebest");   //WiFi connection
+    const char *ssid = "ALEKSNET";
+    WiFi.begin(ssid, "ekvatorthebest");   //WiFi connection
 
     while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
         delay(500);
         Serial.println("Waiting for connection");
     }
+    
+    Serial.println("");
+    Serial.print("Connected to ");  //  "Подключились к "
+    Serial.println(ssid);
+    Serial.print("IP address: ");  //  "IP-адрес: "
+    Serial.println(WiFi.localIP());
+
+    if (mdns.begin("esp8266", WiFi.localIP())) {
+        Serial.println("MDNS responder started");
+        //  "Запущен MDNSresponder"
+    }
+
+    server.on("/", [](){
+        server.send(200, "text/html", measuresToString());
+    });
+    server.begin();
 
     for (auto &reading : measures) {
         reading = (Measure) {-1, -1, -1};
-    };
+    }
+}
+
+void printMeasures() {
+    Serial.println(measuresToString());
 }
 
 void loop() {
@@ -65,28 +95,24 @@ void loop() {
     PmResult pm = sds.queryPm();
     if (pm.isOk()) {
 
-        Serial.println("");
+        Serial.println();
         Serial.println("----------------------");
-        Serial.println("");
+        Serial.println();
 
-        if (++currentReadingIndex <= ARRAY_LENGTH - 1 && !shiftArray) {
+        if (++currentReadingIndex <= ARRAY_LENGTH - 1 && firstPass) {
             measures[currentReadingIndex] = {now(), pm.pm25, pm.pm10};
         } else {
-            if (!shiftArray) {
-                shiftArray = true;
-            }
             for (int i1 = 1, i2 = 0; i1 < ARRAY_LENGTH; i1++, i2++) {
+                // Shift all the array's content on one position
                 measures[i2] = measures[i1];
             }
             measures[ARRAY_LENGTH - 1] = {now(), pm.pm25, pm.pm10};
-        }
-
-        for (auto &measure : measures) {
-            time_t currTime = measure.measureTime;
-            if (currTime != -1) {
-                printMeasure(measure);
+            if (firstPass) {
+                firstPass = false;
             }
         }
+
+        printMeasures();
 
 //        HTTPClient http;    //Declare object of class HTTPClient
 //        http.begin(url);
