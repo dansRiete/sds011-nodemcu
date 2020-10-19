908803183eb6e4a1fdeecabb959310312ba3bfec
Alex Kuzko <okuzk@softserveinc.com>
2020-10-19 19:09:36 +0300

    Simplified version
    (cherry picked from commit d429a3e6d0689d408717123efb963b4b1eae24dd)

--- src/main.cpp
+++ src/main.cpp
@@ -38,66 +38,10 @@ void setup() {
 }
 
 void loop(){
-    WiFiClient client = server.available();               // Получаем данные, посылаемые клиентом
-
-    if (client) {
-        Serial.println("New Client.");                      // Отправка "Новый клиент"
-        String currentLine = "";                            // Создаем строку для хранения входящих данных от клиента
-        while (client.connected()) {                        // Пока есть соединение с клиентом
-            if (client.available()) {                         // Если клиент активен
-                char c = client.read();                         // Считываем посылаемую информацию в переменную "с"
-                Serial.write(c);                                // Отправка в Serial port
-                header += c;
-                if (c == '\n') {                                // Вывод HTML страницы
-                    if (currentLine.length() == 0) {
-                        client.println("HTTP/1.1 200 OK");          // Стандартный заголовок HT
-                        client.println("Content-type:text/html ");
-                        client.println("Connection: close");        // Соединение будет закрыто после завершения ответа
-                        client.println("Refresh: 10");              // Автоматическое обновление каждые 10 сек
-                        client.println();
-
-                        client.println("<!DOCTYPE html><html>");    // Веб-страница создается с использованием HTML
-                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-                        client.println("<meta charset='UTF-8'>");   // Делаем русскую кодировку
-                        client.println("<link rel=\"icon\" href=\"data:,\">");
-
-                        client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial;}");
-                        client.println("table { border-collapse: collapse; width:40%; margin-left:auto; margin-right:auto; }");
-                        client.println("th { padding: 12px; background-color: #0043af; color: white; }");
-                        client.println("tr { border: 1px solid #ddd; padding: 12px; }");
-                        client.println("tr:hover { background-color: #bcbcbc; }");
-                        client.println("td { border: none; padding: 12px; }");
-                        client.println(".sensor { color:white; font-weight: bold; background-color: #bcbcbc; padding: 1px; }");
-
-                        client.println("</style></head><body><h1>Метеостанция на BME280 и ESP8266</h1>");
-                        client.println("<table><tr><th>Параметр</th><th>Показания</th></tr>");
-                        client.println("<tr><td>Температура</td><td><span class=\"sensor\">");
-                        client.println(bme.readTemperature());
-                        client.println(" *C</span></td></tr>");
-                        client.println("<tr><td>Давление</td><td><span class=\"sensor\">");
-                        client.println(bme.readPressure() / 100.0F);
-                        client.println(" hPa</span></td></tr>");
-                        client.println("<tr><td>Приблизительная высота над уровнем моря</td><td><span class=\"sensor\">");
-                        client.println(bme.readAltitude(SEALEVELPRESSURE_HPA));
-                        client.println(" m</span></td></tr>");
-                        client.println("<tr><td>Влажность</td><td><span class=\"sensor\">");
-                        client.println(bme.readHumidity());
-                        client.println(" %</span></td></tr>");
-                        client.println("</body></html>");
-
-                        client.println();
-                        break;
-                    } else {
-                        currentLine = "";
-                    }
-                } else if (c != '\r') {
-                    currentLine += c;
-                }
-            }
-        }
-        header = "";
-        client.stop();
-        Serial.println("Client disconnected.");
-        Serial.println("");
-    }
+    delay(1000);
+    Serial.print("Temp: ");
+    Serial.println(bme.readTemperature());
+    Serial.print("Humid: ");
+    Serial.println(bme.readHumidity());
 }
+
